31bdd7c65dee2dca757287e6b4fae99ea097b024
Harpreet.Singh <harpreet.singh@ivycomptech.com>
2024-05-02 20:03:24 +0530

    chore: Add FormsModule to AppModule for task form functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodolistComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodolistComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrl: './todolist.component.css'
+  styleUrl: './todolist.component.css',
 })
 export class TodolistComponent {
+  taskArray = [{ taskName: 'Task 1', isCompleted: true }];
 
+  onSubmit(taskForm: NgForm) {
+    console.log(taskForm);
+    this.taskArray.push({
+      taskName: taskForm.value.task,
+      // taskName: taskForm.controls['task'].value,
+      isCompleted: false,
+    });
+    taskForm.reset();
+  }
+
+  onDelete(index: number) {
+    this.taskArray.splice(index, 1);
+  }
 }
